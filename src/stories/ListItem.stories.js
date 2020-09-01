37ea25217824140f86906bf80af3abd3b0b3d421
iver gentz <ivergentz@gmail.com>
2020-09-01 15:54:58 +0200

    Jest initialization

--- src/stories/ListItem.stories.js
+++ src/stories/ListItem.stories.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import  {ListItem} from '../dashboardPage/ListItem'
+import  ListItem from '../dashboardPage/ListItem'
+import GlobalStyles from '../GlobalStyles'
+
 
 export default {
   title: 'Rides',
   component: ListItem,
 }
 
-const Template = (args) => <ListItem {...args}/>
+const Template = (args) => 
+    <ListItem {...args}/> 
 
-export const Item = Template.bind({ListItem});
+export const Item = Template.bind({});
