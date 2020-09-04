6260198238a8b77562f0a35939e4d560707bbb5b
iver gentz <ivergentz@gmail.com>
2020-09-04 11:53:19 +0200

    Modiefied toggle logic

--- src/search/Search.js
+++ src/search/Search.js
@@ -1,33 +1,21 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
 import search from '../assets/search.svg'
-import ListItem from '../dashboardPage/ListItem'
 
 export default function Search() {
   const [isToggled, setIsToggled] = useState(false)
-  const [searchIsActive, setSearchisAcitve] = useState(true)
 
   return (  
     <>
       <Image src={search} onClick={toggleSearchBar} alt="search icon" width="30" height="30" ></Image>
       {isToggled && <Input placeholder="Fahrt suchen" ></Input>
-        
          }
-
     </>
     ) 
 
-    function hideList(){
-      setSearchisAcitve(!searchIsActive)
-    }
-
   function toggleSearchBar() {
     setIsToggled(!isToggled)
-    hideList()
-    } 
-
-  
-  
+    }   
 }
 
 const Image = styled.img`
