1afa0f7f9b22edbaba7967e6318bb3327b1c9298
iver gentz <ivergentz@gmail.com>
2020-09-10 17:22:55 +0200

    Initalize cypress testing

---
+++ cypress/integration/dashboardPage.spec.js
@@ -0,0 +1,14 @@
+  /// <reference types="Cypress" />
+
+  context ('DashboardPage', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000')
+    })
+    
+    
+    it('has the right App title', () => {
+      cy.title().should('contain','Riide')
+      }) 
+
+  
+  
