b51fb437607af304d6c4044e51ffc8d20fef21fd
iver gentz <ivergentz@gmail.com>
2020-09-11 15:46:39 +0200

    Merge issues fixed

--- src/dashboardPage/RidesList.js
+++ src/dashboardPage/RidesList.js
@@ -1,24 +1,14 @@
 import React from 'react'
 import ListItem from './RidesListItem'
 
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 export default function RidesList({rides}) {
   return (
     <>
     {rides.map((rides) => (
       <ListItem 
-<<<<<<< HEAD
-        key={rides._id}
-        {...rides}
-     />
-=======
         key={rides.id}
         {...rides}
       />
->>>>>>> master
     ))}
     </>
   )
